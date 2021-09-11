49b1d79902291593ab3ef9cda5b8c8e37f0f1b74
Wolfgang Hennerbichler <wogri@github.com>
2021-09-11 22:18:34 +0200

    move buffer to bbox code, add json descriptions to temperature

--- temperature/temperature.go
+++ temperature/temperature.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Temperature struct {
-	Temperature float64
-	BHiveID     string //BHiveID is usually the Mac address of the raspberry pi in the bHive.
-	SensorID    string
-	Timestamp   int64
-	Error       string
+	Temperature float64 `json:"temperature,omitempty"`
+	BHiveID     string  `json:"bhiveId,omitempty"` //BHiveID is usually the Mac address of the raspberry pi in the bHive.
+	SensorID    string  `json:"sensorId,omitempty"`
+	Timestamp   int64   `json:"timestamp,omitempty"` //TODO: rename to epoch!
+	Error       string  `json:"error,omitempty"`
 }
 
 func (t *Temperature) String() ([]byte, error) {
