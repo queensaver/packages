01353cfc816a558a376f8f9b73fefeace1224d82
Wolfgang Hennerbichler <wogri@github.com>
2021-09-28 22:01:07 +0200

    implement the zap logger

--- config/config.go
+++ config/config.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/queensaver/packages/logger"
 )
 
 type Config struct {
@@ -109,7 +111,7 @@ func Get(addr string, token string) (*Config, error) {
 	}
 	req.Header.Set("BBoxID", mac)
 
-	fmt.Printf("Requesting config for BBox ID %s", mac)
+	logger.Info("Requesting config for BBox ID %s", mac)
 	res, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
--- config/go.mod
+++ config/go.mod
@@ -1,3 +1,5 @@
 module github.com/queensaver/packages/config
 
 go 1.16
+
+require github.com/queensaver/packages/logger v0.0.0-20210924194626-eecb7227e71c
