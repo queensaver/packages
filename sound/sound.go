5a05e7770716e33467d764efc91d98f92635deab
Wolfgang Hennerbichler <wogri@github.com>
2021-10-05 09:18:50 +0200

    simplify / revert the sound struct, fuck it we will do it in memory

--- sound/sound.go
+++ sound/sound.go
@@ -2,25 +2,22 @@ package sound
 
 import (
 	"encoding/json"
-	"os"
 
 	"github.com/google/uuid"
 )
 
 type Sound struct {
-	Sound []byte `json:"sound,omitempty"`
-	//BHiveID is usually the Mac address of the raspberry pi in the bHive.
-	BhiveId    string   `json:"bhiveId,omitempty"`
-	Epoch      int64    `json:"epoch,omitempty"`
-	Error      string   `json:"error,omitempty"`
-	UUID       string   `json:"uuid,omitempty"`
-	Duration   int      `json:"duration,omitempty"`
-	FileHandle *os.File `json:"-"`
+	Sound    string `json:"sound,omitempty"`
+	BhiveId  string `json:"bhiveId,omitempty"` //BHiveID is usually the Mac address of the raspberry pi in the bHive.
+	Epoch    int64  `json:"epoch,omitempty"`
+	Error    string `json:"error,omitempty"`
+	UUID     string `json:"uuid,omitempty"`
+	Duration int    `json:"duration,omitempty"`
 }
 
 func (s *Sound) String() ([]byte, error) {
 	copy := *s
-	copy.Sound = nil
+	copy.Sound = ""
 	return json.MarshalIndent(copy, "", "  ")
 }
 
