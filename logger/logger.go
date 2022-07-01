d3fa7e82d60f71b30ab7144b480aa1f3909df060
Wolfgang Hennerbichler <wogri@wogri.com>
2022-07-01 20:45:09 +0200

    logger now also logs line numbers - maybe for real?

--- logger/logger.go
+++ logger/logger.go
@@ -28,7 +28,7 @@ func init() {
 		zaplogfmt.NewEncoder(config),
 		os.Stdout,
 		level,
-	), zap.AddCaller())
+	), zap.AddCaller(), zap.AddCallerSkip(1))
 
 	defer logger.Sync()
 	sugar = logger.Sugar()
